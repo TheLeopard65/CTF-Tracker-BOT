616367c1527aa75271000435b96b23ea3e4d8d9d
Yasir Mehmood <152008995+TheLeopard65@users.noreply.github.com>
2024-11-15 15:04:28 +0500

    Update CTF-Tracker.js

--- CTF-Tracker.js
+++ CTF-Tracker.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch');
 const NodeCache = require('node-cache');
 const cache = new NodeCache({ stdTTL: 60 * 60 * 24 });
 
-const settings = { token: process.env.TOKEN, ownerId: process.env.OWNERID, color: '#0276C9', timezone: 'Asia/Karachi' };
+const settings = { token: process.env.TOKEN, color: '#0276C9', timezone: 'Asia/Karachi' };
 global.config = settings;
 
 const client = new djs.Client({
@@ -18,21 +18,16 @@ const fetchAndCache = async (key, url, retries = 3) => {
     const cachedData = cache.get(key);
     if (cachedData) { return cachedData; }
     try {
-        console.log(`Fetching data from ${url}, retries left: ${retries}`);
         const response = await fetch(url, { timeout: 15000 });
         if (response.status !== 200) return null;
         const data = await response.json();
         cache.set(key, data);
         return data;
     } catch (error) {
-        if (retries > 0 && error.code === 'ECONNRESET') { 
-            return fetchAndCache(key, url, retries - 1); 
-        }
-        console.error(`Error fetching data from ${url}:`, error.message);
+        if (retries > 0 && error.code === 'ECONNRESET') { return fetchAndCache(key, url, retries - 1); }
         return null;
     }
 };
-
 
 const getTeam = async query => {
     if (query.match(/^[0-9]+$/)) {
@@ -60,14 +55,7 @@ const getEventsByTeam = async team => {
     for (const eventId in events) {
         const event = events[eventId];
         const teamScore = event.scores.find(score => score.team_id == team);
-        if (teamScore) {
-            teamEvents.push({
-                eventId,
-                title: event.title,
-                points: teamScore.points,
-                place: teamScore.place,
-            });
-        }
+        if (teamScore) { teamEvents.push({ eventId, title: event.title, points: teamScore.points, place: teamScore.place, }); }
     }
     if (teamEvents.some(event => times.find(time => time.id == event.eventId)?.start == undefined)) { return null; }
     teamEvents.sort((a, b) => {
@@ -86,15 +74,11 @@ const getUpcomingEvents = async () => {
     startDate.setDate(startDate.getDate() + ((1 - startDate.getDay()) % 7));
     startDate.setHours(3, 0, 0, 0);
     const endDate = new Date(startDate);
-    endDate.setDate(new Date().getDate() + 8);
+    endDate.setDate(new Date().getDate() + 60);
     endDate.setHours(3, 0, 0, 0);
     const upcomingEvents = events.filter(event => {
         const startTime = new Date(event.start);
-        return (
-            startTime >= startDate &&
-            startTime <= endDate &&
-            (event.onsite || event.location.includes('Online') || event.location == '')
-        );
+        return ( startTime >= startDate && startTime <= endDate && (event.onsite || event.location.includes('Online') || event.location == '') );
     });
     return upcomingEvents || null;
 };
@@ -108,9 +92,9 @@ const registerCommands = async () => {
 
         new djs.SlashCommandBuilder()
             .setName('newctfs')
-            .setDescription('Lists the top upcoming CTF Events on the CTFTime.')
+            .setDescription('Lists the Top Upcoming CTF Events on the CTFTime.')
             .addIntegerOption(option =>
-                option.setName('limit')
+                option.setName('count')
                     .setDescription('Limits number of upcoming events to display.')
                     .setRequired(false)
                     .setMinValue(1)
@@ -127,11 +111,6 @@ const registerCommands = async () => {
                         { name: 'TEAMS', value: 'participants' },
                         { name: 'DURATION', value: 'duration' }
                     )
-            )
-            .addBooleanOption(option =>
-                option.setName('show_description')
-                    .setDescription('Show event descriptions in the result.')
-                    .setRequired(false)
             )
             .toJSON(),
 
@@ -158,14 +137,14 @@ const registerCommands = async () => {
     ];
 
     const rest = new djs.REST({ version: '10' }).setToken(client.token);
-    try {	const slashCommands = await rest.put(djs.Routes.applicationCommands(client.user.id), { body: commandsList });	}
-    catch (error) {	console.error('ERROR : COULDN\'T REGISTER COMMANDS : ', error);	}
+    try { const slashCommands = await rest.put(djs.Routes.applicationCommands(client.user.id), { body: commandsList }); }
+    catch (error) { console.error('ERROR : COULDN\'T REGISTER COMMANDS : ', error); }
 };
 
 const helpCommand = async interaction => {
     const embed = new djs.EmbedBuilder()
         .setColor(global.config.color)
-        .setTitle('Commands List')
+        .setTitle('#####@ - COMMANDS LIST - @#####')
         .setDescription(
             `**/help** - Display this Help Message to find out what the BOT Does!\n` +
             `**/newctfs** - Lists the top upcoming CTF Events on the CTFTime.\n` +
@@ -178,12 +157,11 @@ const helpCommand = async interaction => {
 
 const newctfsCommand = async interaction => {
     await interaction.deferReply();
-    const limit = interaction.options.getInteger('limit') || 10;
-    const sort = interaction.options.getString('sort') || 'weight';
-    const showDescription = interaction.options.getBoolean('show_description') === null ? true : interaction.options.getBoolean('show_description');
+    const limit = interaction.options.getInteger('count') || 10;
+    const sort = interaction.options.getString('sort') || 'start';
     try {
         const upcomingEvents = await getUpcomingEvents();
-        if (!upcomingEvents) {	return interaction.editReply({ content: 'ERROR : Issue while fetching upcoming CTF CTF Events on CTFTime' });	}
+        if (!upcomingEvents) { return interaction.editReply({ content: 'ERROR : Issue while fetching upcoming CTF Events on CTFTIME.ORG' }); }
         const events = upcomingEvents
             .sort((a, b) => {
                 if (sort === 'weight') return b.weight - a.weight;
@@ -197,55 +175,52 @@ const newctfsCommand = async interaction => {
 
         const embed = new djs.EmbedBuilder()
             .setColor(global.config.color)
-            .setTitle(' ##### TOP UPCOMING CTF EVENTS ON CTFTIME ##### ')
+            .setTitle(' #####@ - TOP UPCOMING CTF EVENTS ON CTFTIME.ORG - @##### ')
             .setURL('https://ctftime.org/event/list/upcoming')
             .setDescription('Here are the Top Upcoming Online CTFs on CTFTime for you to play:');
 
         let characters = 0;
-        for (let i = 0; i < events.length && characters + 1000 < 5500; i++) {
+        for (let i = 0; i < events.length && characters + 1000 < 30000; i++) {
             const event = events[i];
             const startTime = new Date(event.start);
             const endTime = new Date(event.finish);
 
             embed.addFields({
-                name: `${i + 1}. ${event.title}`,
-                value: `**Start**: <t:${Math.floor(startTime.getTime() / 1000)}:f>\n**End**: <t:${Math.floor(endTime.getTime() / 1000)}:f>\n**Weight**: ${event.weight}`,
+                name: `${i + 1}. [${event.title}](https://ctftime.org/event/${event.id})`,
+                value: `**TIMING**: FROM <t:${Math.floor(startTime.getTime() / 1000)}:f> TO <t:${Math.floor(endTime.getTime() / 1000)}:f>\n`,
             });
             characters += 1000;
         }
-
         await interaction.editReply({ embeds: [embed] });
-    } catch (error) {
-        await interaction.editReply({ content: 'ERROR: Timed-Out while fetching upcoming CTF Events. (PLEASE TRY AGAIN LATER)' });
-    }
+    } catch (error) { await interaction.editReply({ content: 'ERROR: Timed-Out while fetching upcoming CTF Events. (PLEASE TRY AGAIN LATER)' }); }
 };
 
 const teaminfoCommand = async interaction => {
     await interaction.deferReply();
     const query = interaction.options.getString('teamid');
-    if (!query) {	return await interaction.editReply({ content: 'SYNTAX: Provide a team name or ID to search for.', ephemeral: true });	}
+    if (!query) { return await interaction.editReply({ content: 'SYNTAX: Provide a team name or ID to search for.', ephemeral: true }); }
     try {
         const team = await getTeam(query);
-        if (!team || !team.id) {	return await interaction.editReply({ content: `ISSUE: TEAM "${query}" NOT FOUND. (PLEASE CHECK YOUR INPUT)`, ephemeral: true });	}
+        if (!team || !team.id) { return await interaction.editReply({ content: `ISSUE: TEAM "${query}" NOT FOUND. (PLEASE CHECK YOUR INPUT)`, ephemeral: true }); }
         const members = team.aliases && team.aliases.length > 0 ? team.aliases.join(', ') : 'N/A';
         const events = await getEventsByTeam(team.id);
-        if (!events || events.length === 0) {	return await interaction.editReply({ content: `ISSUE: NO CTF EVENTS FOUND FOR TEAM "${team.name}".`, ephemeral: true });	}
+        if (!events || events.length === 0) { return await interaction.editReply({ content: `ISSUE: No CTF Events Found for Team "${team.name}".`, ephemeral: true }); }
 
         const embed = new djs.EmbedBuilder()
             .setColor(global.config.color)
             .setTitle(`#####@ - ${team.name} TEAM INFORMATION - @#####`)
             .setURL(`https://ctftime.org/team/${team.id}/`)
             .addFields(
-                { name: 'NAME:', value: `${team.name}`, inline: true },
+                { name: 'NAME:', value: `${team.name || 'N/A'}`, inline: true },
                 { name: 'Country:', value: `${team.country || 'N/A'}`, inline: true },
                 { name: 'TEAM-ID:', value: `${team.id}`, inline: true },
-                { name: 'NATIONAL-RANK:', value: `${team.rating && team.rating['2024'] ? team.rating['2024'].country_place.toString() : 'N/A'}`, inline: true },
+                { name: 'NATIONAL-RANK:', value: `${team.rating && team.rating["2024"] && team.rating["2024"].country_place ? team.rating["2024"].country_place.toString() : "N/A"}`, inline: true },
                 { name: 'TEAM MEMBERS:', value: `${members}`, inline: true }
             )
             .setFooter({ text: 'NOTE: DATA PROVIDED BY CTFTIME.ORG' })
             .setTimestamp();
 
-		const eventFields = events.slice(0, 5).map((event, index) => {
+        const eventFields = events.slice(0, 5).map((event, index) => {
             const eventTitle = event.title || 'No Title';
             const eventPoints = event.points || 'No Points';
             const eventRank = event.place || 'N/A';
@@ -266,36 +241,41 @@ const teaminfoCommand = async interaction => {
 
 let teamIds = [];
 const setteamsCommand = async interaction => {
-    if (interaction.user.id !== global.config.ownerId) {	return interaction.reply({ content: 'ISSUE: ADMINISTRATOR PRIVILEGES NEEDED!!!', ephemeral: true });}
+    const member = interaction.guild.members.cache.get(interaction.user.id);
+    if (!member || !member.permissions.has(djs.PermissionFlagsBits.Administrator)) { return interaction.reply({ content: 'ISSUE: ADMINISTRATOR PRIVILEGES REQUIRED!!!', ephemeral: true }); }
     const teamsInput = interaction.options.getString('teams');
     teamIds = teamsInput.split(',').map(t => t.trim());
-    await interaction.reply({ content: `SUCCESS: TEAM IDs HAVE BEEN SET TO: ${teamIds.join(', ')}` });
+    await interaction.reply({ content: `SUCCESS: Team IDs have been Set to ${teamIds.join(', ')} for the Comparison` });
 };
 
 const compareCommand = async interaction => {
-    if (teamIds.length === 0) { return interaction.reply({ content: 'No teams have been set yet. Use /setteams to set the teams.', ephemeral: true });  }
+    if (teamIds.length === 0) { return interaction.reply({ content: 'ISSUE: Use /setteams to set the teams.', ephemeral: true }); }
+    await interaction.deferReply();
     const teamData = [];
     for (const teamQuery of teamIds) {
         const team = await getTeam(teamQuery);
-        if (!team) return interaction.reply({ content: `ISSUE: TEAM ${teamQuery} NOT FOUND`, ephemeral: true });
+        if (!team) { return interaction.editReply({ content: `ISSUE: TEAM ${teamQuery} NOT FOUND`, ephemeral: true }); }
         const results = await getEventsByTeam(team.id);
-        if (!results.length) return interaction.reply({ content: `ISSUE: NO EVENTS FOUND ${team.name}.`, ephemeral: true });
-        const totalPoints = results.slice(0, 5).reduce((sum, event) => sum + parseInt(event.points.replace(' pts', ''), 10), 0);
-        teamData.push({ name: team.name, totalPoints, results });
+        if (!results || results.length === 0) { return interaction.editReply({ content: `ISSUE: No CTF Events Found for Team ${team.name}.`, ephemeral: true }); }
+        const latestResults = results.slice(0, 5);
+        const totalPoints = latestResults.reduce((sum, event) => {
+            const points = parseInt(event.points.replace(' POINTS', ''), 10);
+            return sum + (isNaN(points) ? 0 : points);
+        }, 0);
+        teamData.push({ name: team.name, totalPoints, results: latestResults });
     }
     teamData.sort((a, b) => b.totalPoints - a.totalPoints);
     const embed = new djs.EmbedBuilder()
         .setColor(global.config.color)
-        .setTitle(' ~~~# OUR CTF TEAMs COMPARISON BASED ON LAST 5 CTFs #~~~ ')
-        .setDescription('Here is the scoreboard for the teams based on their latest CTF performance.');
+        .setTitle('##@- CTF TEAMS COMPARISON BASED ON LAST 5 CTFs -@##');
 
     teamData.forEach((team, index) => {
         embed.addFields({
-            name: `${index + 1}. ${team.name}`,
-            value: `Total Points: ${team.totalPoints}\nResults: ${team.results.map(e => `${e.title}: ${e.points}`).join('\n')}`,
+            name: `${index + 1}. ${team.name} (TP: ${team.totalPoints})\n`, inline: true,
+            value: `\n### RECENT EVENT RESULTS ###\n` + team.results.map(e => `${index + 1}) ${e.title}: ${e.points}`).join('\n') || 'No Results Available',
         });
     });
-    await interaction.reply({ embeds: [embed] });
+    await interaction.editReply({ embeds: [embed] });
 };
 
 client.once('ready', async () => {	await registerCommands();	});
@@ -308,9 +288,7 @@ client.on('interactionCreate', async interaction => {
             else if (interaction.commandName === 'setteams') {	await setteamsCommand(interaction);	}
             else if (interaction.commandName === 'compare') {	await compareCommand(interaction);	}
         } catch (err) {
-            if (!interaction.replied) {
-                await interaction.reply({ content: 'ERROR: COULDN\'T PROCESS YOUR COMMANDS', ephemeral: true });
-            }
+            if (!interaction.replied) { await interaction.reply({ content: 'ERROR: !COULDN\'T PROCESS YOUR COMMANDS! SORRY!!', ephemeral: true }); }
         }
     }
 });
